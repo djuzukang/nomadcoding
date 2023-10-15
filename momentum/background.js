168dc7019dde0fd35072623b29c3f2b43e32a74e
djuzukang <juzubbans@gmail.com>
2023-10-16 01:18:15 +0900

    css adjustment

--- momentum/background.js
+++ momentum/background.js
@@ -10,7 +10,10 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement('img');
-bgImage.src = `img/${chosenImage}`;
+// const bgImage = document.createElement('img');
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
